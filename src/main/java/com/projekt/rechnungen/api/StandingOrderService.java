4e8955aa1b7c8591cbf596cc1355157ab18fcfca
lennardbernet <lennard758@gmail.com>
2020-04-22 13:01:20 +0200

    changed query to persist

--- src/main/java/com/projekt/rechnungen/api/StandingOrderService.java
+++ src/main/java/com/projekt/rechnungen/api/StandingOrderService.java
@@ -29,17 +29,7 @@ public class StandingOrderService {
         List<StandingOrder> list = null;
         try{
             System.out.println(standingOrder.toString());
-            list = getAllStandingOrders();
             em.persist(standingOrder);
-            em.createNativeQuery("insert into StandingOrder (adress_idfk, amount,executions,firstname, iban, repetitionrate, lastname) values (?,?,?,?,?,?,?)")
-                    .setParameter(1, standingOrder.getAdress().getAdressId())
-                    .setParameter(2, standingOrder.getAmount())
-                    .setParameter(3,standingOrder.getExecutions())
-                    .setParameter(4, standingOrder.getFirstname())
-                    .setParameter(5,standingOrder.getIban())
-                    .setParameter(6,standingOrder.getRepetitionrate())
-                    .setParameter(7,standingOrder.getLastname())
-                    .executeUpdate();
             list = getAllStandingOrders();
         }catch (IllegalArgumentException e){
             e.printStackTrace();
